59aa4fa4ef15eb1daccaafac163eb79f367aaa4a
27kUltraHd <nhaison@pdx.edu>
2020-07-13 12:05:04 -0700

    Updates

---
+++ precache-manifest.67a7381784e7f0505487d91e23e04215.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed170121fba0c70b063e5a8538b6b0b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "645ffef56ed80063be8d",
+    "url": "/portfolio/static/css/2.3295f513.chunk.css"
+  },
+  {
+    "revision": "814ff18d150c6264126f",
+    "url": "/portfolio/static/css/main.873a5746.chunk.css"
+  },
+  {
+    "revision": "645ffef56ed80063be8d",
+    "url": "/portfolio/static/js/2.7373836c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.7373836c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814ff18d150c6264126f",
+    "url": "/portfolio/static/js/main.43db62e5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2aa759e96caee4ba8eefaac95c405695",
+    "url": "/portfolio/static/media/opener.2aa759e9.jpg"
+  }
+]);
