c078b5ab0808ee37ed4d093b60557673431db5c7
27kUltraHd <nhaison@pdx.edu>
2020-07-23 17:17:33 -0700

    Updates

---
+++ precache-manifest.7df2d2ea090c06d06cc2625a71f8dc3d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d44ceb10b162f85686d6de1fab79a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c6dac4ccbc0bf86a961",
+    "url": "/portfolio/static/css/2.eb3137df.chunk.css"
+  },
+  {
+    "revision": "76b6374203d7c1334b24",
+    "url": "/portfolio/static/css/main.5a0701a9.chunk.css"
+  },
+  {
+    "revision": "7c6dac4ccbc0bf86a961",
+    "url": "/portfolio/static/js/2.0f8ebbc3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.0f8ebbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b6374203d7c1334b24",
+    "url": "/portfolio/static/js/main.5df73090.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d8556edf9621e42c9b147578a8c6fd1c",
+    "url": "/portfolio/static/media/jonathan-simcoe.d8556edf.jpg"
+  },
+  {
+    "revision": "e3537ca594dace0b1b7a9c2120518cc2",
+    "url": "/portfolio/static/media/me_selfie.e3537ca5.JPEG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23b4e0b15956c670e1708eba01889674.js"
+  "/portfolio/precache-manifest.7df2d2ea090c06d06cc2625a71f8dc3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
