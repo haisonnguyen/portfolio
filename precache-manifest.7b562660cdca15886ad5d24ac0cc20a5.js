e8c1122ab4a511c54bff504a6b58435132f8cb40
27kUltraHd <nhaison@pdx.edu>
2020-07-24 11:11:39 -0700

    Updates

---
+++ precache-manifest.7b562660cdca15886ad5d24ac0cc20a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c30f881aa54cd2675a42b92aaafaf9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7c6dac4ccbc0bf86a961",
+    "url": "/portfolio/static/css/2.eb3137df.chunk.css"
+  },
+  {
+    "revision": "878b156d56e94d8ae9a4",
+    "url": "/portfolio/static/css/main.f9364bad.chunk.css"
+  },
+  {
+    "revision": "7c6dac4ccbc0bf86a961",
+    "url": "/portfolio/static/js/2.0f8ebbc3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.0f8ebbc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878b156d56e94d8ae9a4",
+    "url": "/portfolio/static/js/main.5df73090.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d8556edf9621e42c9b147578a8c6fd1c",
+    "url": "/portfolio/static/media/jonathan-simcoe.d8556edf.jpg"
+  },
+  {
+    "revision": "e3537ca594dace0b1b7a9c2120518cc2",
+    "url": "/portfolio/static/media/me_selfie.e3537ca5.JPEG"
+  }
+]);
